cb9231cb645ecac383f89b81199d9c0c9fa1e79f
sasharosehalpern <sashahalpern626@gmail.com>
2024-11-11 15:25:10 +0200

    added post and user controllers

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    @Get('/@:username')
+    getUserByUsername(@Param() param): string {
+        return `details of username = ${param.username}`;
+    }
+
+    @Get('/@:userid')
+    getUserByUserid(@Param() param): string {
+        return `details of user id = ${param.userid}`;
+    }
+
+    @Post('/')
+    createNewUser(): string {
+      return 'new user created';
+    }
+
+    @Patch('/:userid')
+    updateUserDetails(@Param() param): string {
+        return `details of user (id = ${param.userid}) updated`;
+    }
+
+    @Put('/:userid/follow')
+    followUser(): string {
+        return 'you followed user';
+    }
+
+    @Delete('/:userid/follow')
+    unfollowUser(): string {
+        return 'you unfollowed user';
+    }
+
+    @Get('/:userid/followers')
+    getFollowersOfUser(): string {
+        return 'get followers of user';
+    }
+
+    @Get('/:userid/followees')
+    getFolloweesOfUser(): string {
+        return 'get followees of given user';
+    }
+}
